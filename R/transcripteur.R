5f52a66758c8e1db394dbb79c1c757a03aa8b766
Aurelie Frechet <aurelie.frechet@gmail.com>
2020-03-04 13:51:19 +0100

    init

---
+++ R/transcripteur.R
@@ -0,0 +1,12 @@
+#' Transcripteur
+#'
+#' @description transcrit du code SAS en R
+#'
+#' @param input fichier SAS
+#' @param output fichier R
+#'
+#' @export
+#'
+transcripteur <- function(input, output){
+
+}
