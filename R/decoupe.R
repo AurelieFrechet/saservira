e91d2801620411fbb59d491e6ff8e234f0f0abbf
Aurelie Frechet <aurelie.frechet@gmail.com>
2020-03-10 07:52:59 +0100

    documentation

--- R/decoupe.R
+++ R/decoupe.R
@@ -1,7 +1,21 @@
-library(stringr)
-library(stringi)
-library(dplyr)
-
+#' Decoupe code SAS
+#'
+#' @import stringr
+#' @import dplyr
+#'
+#' @description identifie les blocs de procédures/commentaires/étapes data/etc,
+#' les découpe et indique leur position dans le code d'origine
+#'
+#' @param code_sas : code SAS en entrée, non découpé par lignes
+#'
+#' @return liste d'élements : place, texte et id
+#' place : localisation du code extrait
+#' texte : code extrait
+#' id : identifiant du code extrait, premier mot ou groupe de mot du bloc,
+#' identifiant la procédure ou le commentaire, etc.
+#' @export
+#'
+#' @examples
 decouper_SAS <- function(code_sas) {
   # PROCEDURES : proc mot [...] run;/quit;
   locate_proc <- str_locate_all(
