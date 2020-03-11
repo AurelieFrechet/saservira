d0df6d7df61b2363537882343425c6ba6f6be99f
Aurelie Frechet <aurelie.frechet@gmail.com>
2020-03-11 17:07:49 +0100

    detext sql blocs

---
+++ R/sasr_sql.R
@@ -0,0 +1,28 @@
+sasr_sql <- function(code_sas){
+  # SELECT
+    sql_select <- str_extract_all(code_sas,
+                              regex(pattern   = "select [a-zA-Z0-9.]+(\\s?,\\s?[a-zA-Z0-9.]+)*|select \\*",
+                                    multiline = TRUE))[[1]]
+
+  # FROM
+  sql_from <- str_extract_all(code_sas,
+                              regex(pattern   = "from [a-zA-Z0-9.]+(\\s?,\\s?[a-zA-Z0-9.]+)*",
+                                    multiline = TRUE))[[1]]
+
+  # WHERE
+  sql_where <- str_extract_all(code_sas,
+                             regex(pattern   = "where ([a-zA-Z0-9.\\s]+(=|is not|is))*\\s?\\w+",
+                                   multiline = TRUE))[[1]]
+
+  # GROUP BY
+  sql_group_by <- str_extract_all(code_sas,
+                              regex(pattern   = "group by [a-zA-Z0-9.]+(\\s?,\\s?[a-zA-Z0-9.]+)*",
+                                    multiline = TRUE))[[1]]
+
+  # ORDER BY
+  sql_order_by <- str_extract_all(code_sas,
+                              regex(pattern   = "order by [a-zA-Z0-9.]+(\\s?,\\s?[a-zA-Z0-9.]+)*(\\sdescending)?",
+                                    multiline = TRUE))[[1]]
+
+
+  }
