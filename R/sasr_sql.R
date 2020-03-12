f138da95dd5f918bf5e1c35773bdf7f98c527eac
Aurelie Frechet <aurelie.frechet@gmail.com>
2020-03-12 14:11:01 +0100

    nouvelle idee separation par mot cle

--- R/sasr_sql.R
+++ R/sasr_sql.R
@@ -1,11 +1,48 @@
-sasr_sql <- function(code_sas){
+sasr_sql <- function(code_sas) {
+  # Séparer les différentes requêtes ----
+  requetes <- code_sas %>%
+    str_replace(pattern = "proc sql;", replacement = "") %>%
+    str_replace(pattern = "quit;", replacement = "") %>%
+    str_split(pattern = ";") %>%
+    unlist() %>%
+    {
+      .[-which(str_detect(., "^\n$"))]
+    }
+
+  # Couper au niveau des mots clés ----
+  key_words <- c("select",
+                 "from",
+                 "where",
+                 "order by",
+                 "group by",
+                 "limit")
+  pattern_kw <- paste(
+    paste0("(?=", key_words, ")"),
+    collapse = "|")
+
+  requetes_list <- lapply(
+    X = requetes,
+    FUN = function(x) {
+      str_split(string = x,
+                pattern = pattern_kw)[[1]] %>%
+        {
+          .[-which(str_detect(., "^\n"))]
+        } %>%
+        str_replace(pattern = "\n$", replacement = "") %>%
+        str_trim()
+    }
+  )
+
   # SELECT
   sql_select <-
     str_extract_all(
       code_sas,
-      regex(pattern   = "select [a-zA-Z0-9.]+(\\s?,\\s?[a-zA-Z0-9.]+)*|select \\*",
+      regex(pattern   = paste(
+        "select [a-zA-Z0-9.()]+(\\sas [a-zA-Z0-9]+)?(\\s?,\\s?[a-zA-Z0-9.()]+(\\sas [a-zA-Z0-9]+)?)*",
+        "select \\*", sep ="|"),
             multiline = TRUE))[[1]] %>%
-    str_replace(pattern = "select ", replacement = "")
+    str_replace(pattern = "select ", replacement = "") %>%
+    str_split(pattern = "\\s?,\\s?")
 
   # FROM
   sql_from <-
@@ -13,16 +50,17 @@ sasr_sql <- function(code_sas){
       code_sas,
       regex(pattern   = "from [a-zA-Z0-9.]+(\\s?,\\s?[a-zA-Z0-9.]+)*",
             multiline = TRUE))[[1]] %>%
-    str_replace(pattern = "from ", replacement = "")
+    str_replace(pattern = "from ", replacement = "") %>%
+    str_split(pattern = "\\s?,\\s?")
 
   # WHERE
   sql_where <-
     str_extract_all(
       code_sas,
-      regex(pattern   = "where ([a-zA-Z0-9.\\s]+(=|is not|is))*\\s?\\w+",
+      regex(pattern   = "(?=where)[\\s\\S]*?(?=(;|order by|group by))",
             multiline = TRUE))[[1]] %>%
     str_replace(pattern = "where ", replacement = "") %>%
-    str_split(pattern = "\\s?(and|or)\\s?")
+    as.list()
 
   # GROUP BY
   sql_group_by <-
@@ -30,15 +68,19 @@ sasr_sql <- function(code_sas){
       code_sas,
       regex(pattern   = "group by [a-zA-Z0-9.]+(\\s?,\\s?[a-zA-Z0-9.]+)*",
             multiline = TRUE))[[1]] %>%
-    str_replace(pattern = "group by ", replacement = "")
+    str_replace(pattern = "group by ", replacement = "")  %>%
+    str_split(pattern = "\\s?,\\s?")
 
   # ORDER BY
   sql_order_by <-
     str_extract_all(
       code_sas,
-      regex(pattern   = "order by [a-zA-Z0-9.]+(\\sdescending)?(\\s?,\\s?[a-zA-Z0-9.]+(\\sdescending)?)*",
+      regex(pattern   = "order by [a-zA-Z0-9.]+(\\sdesc|\\sasc)?(\\s?,\\s?[a-zA-Z0-9.]+(\\sdesc|\\sasc)?)*",
             multiline = TRUE))[[1]] %>%
-    str_replace(pattern = "order by ", replacement = "")
+    str_replace(pattern = "order by ", replacement = "") %>%
+    str_split(pattern = "\\s?,\\s?")
 
+
+  "[a-zA-Z0-9.\\s]+\s?(=|<|>|<>|<=|>=)\s?[a-zA-Z0-9.\"']+|[a-zA-Z0-9.\\s]+\s(is not|is)\snull|[a-zA-Z0-9.\\s]+\sbetween \w+ and \w+"
 
   }
