c1f7e31693573411f0ebf9317ad342224c74ba59
FRECHET Aurélie (BD Life Sciences) <E633364@dir.ucb-group.com>
2022-11-16 22:57:54 +0100

    detection procs

--- R/get_SAS_procedure.R
+++ R/get_SAS_procedure.R
@@ -1,3 +1,39 @@
-get_SAS_procedure <- function(text){
+#' get SAS procedures
+#' @import stringr
+#' @import magrittr
+#' @description read SAS code, identify procedures and extract their contents
+#' @param text SAS code
+#' @return list of identified procedures
+#' @export
+#'
+#' @examples
+#' get_SAS_procedure(text = "PROC CONTENTS DATA=sample;RUN;")
+get_SAS_procedure <- function(text) {
+  proc <- c("chart", "contents", "corr",
+            "freq", "gchart", "genmod", "glm", "gplot",
+            "means", "plot", "print",
+            "sql", "summary", "transpose", "univariate")
+
+  # Clean Text
+  text <- text %>%
+    tolower() %>%
+    str_squish() %>%
+    str_replace_all(pattern = regex("run\\s?;",
+                                    ignore_case = TRUE),
+                    replacement = "run;") %>%
+    str_replace_all(pattern = regex("quit\\s?;",
+                                    ignore_case = TRUE),
+                    replacement = "quit;")
+
+
+  # Detect procedures
+  procedures <- lapply(
+    X = proc,
+    FUN = function(x)
+      str_match_all(string  = text,
+                    pattern = paste0("proc ",x ,"\\s*(.*?)\\s*run;"))[[1]][, 2]
+  )
+
+  names(procedures) <- paste("proc", proc, sep = "_")
 
 }
---
+++ tests/testthat/test-get_SAS_procedure.R
@@ -0,0 +1,65 @@
+# One procedure ----
+test_that("PROC CHART", {
+  ""
+})
+
+test_that("PROC CONTENTS", {
+  text = "PROC CONTENTS DATA=sample;
+          RUN;"
+})
+
+test_that("PROC CORR", {
+  ""
+})
+
+test_that("PROC FREQ", {
+  text = "proc freq data = example1;
+          tables y;
+          run;"
+})
+
+test_that("PROC GCHART", {
+  ""
+})
+
+test_that("PROC GENMOD", {
+  ""
+})
+
+test_that("PROC GLM", {
+  ""
+})
+
+test_that("PROC GPLOT", {
+  ""
+})
+
+test_that("PROC MEANS", {
+  text = "Proc Means Data=SASHelp.cars Q1 Median Q3;
+          Var MSRP Invoice;
+          Run;"
+})
+
+test_that("PROC PLOT", {
+  ""
+})
+
+test_that("PROC PRINT", {
+  ""
+})
+
+test_that("PROC SQL", {
+  ""
+})
+
+test_that("PROC SUMMARY", {
+  ""
+})
+
+test_that("PROC TRANSPOSE", {
+  ""
+})
+
+test_that("PROC UNIVARIATE", {
+  ""
+})
